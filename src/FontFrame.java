1b1861f27627d8ffe6cad5a65c4996895f5ff1b7
Sundarina <sundarina@ukr.net>
2017-01-17 10:59:43 +0200

    Примеры изменения шрифтов и распол элементов в окнах

--- src/FontFrame.java
+++ src/FontFrame.java
@@ -13,15 +13,15 @@ public class FontFrame extends JFrame {
 	public static final int TEXT_ROWS = 10;
 	public static final int TEXT_COLUMNS = 20;
 	
-	private JComboBox<String> face;
+	private JComboBox<String> face; //комбинированный список
 	private JComboBox<Integer> size;
 	
-	private JCheckBox bold;
+	private JCheckBox bold; //флажки
 	private JCheckBox italic;
 	private JTextArea sample;
 
 	public FontFrame() {
-		GridBagLayout layout  = new GridBagLayout();
+		GridBagLayout layout  = new GridBagLayout(); //сеточно контейнерная компоновка
 		setLayout(layout);
 		
 		ActionListener listener = EventHandler.create(ActionListener.class, this, "updateSample");
@@ -44,29 +44,35 @@ public class FontFrame extends JFrame {
 		
 		sample = new JTextArea(TEXT_ROWS, TEXT_COLUMNS);
 		sample.setText("Five little monkeys jumping on the bed, one fell off and bamped his head");
+		sample.setBackground(Color.BLACK);
 		sample.setEnabled(false);
-		sample.setLineWrap(true);
-		sample.setBorder(BorderFactory.createEtchedBorder());
+		sample.setLineWrap(true); //включен авто режим переноса
+		sample.setBorder(BorderFactory.createEtchedBorder()); //рамка пустая
 		
 		//ввести компоненты в сетку, использую служебный класс
 		
-		add(faceLabel, new GBC(0,0).setAnchor(GBC.EAST));
-		add(face, new GBC(1, 0).setFill(GBC.HORIZONTAL).setWeight(100, 0).setInsets(1));
-		add(sizeLabel, new GBC(0, 1).setAnchor(GBC.EAST));
+		//добавл компонентов во фрейм
+		
+		add(faceLabel, new GBC(0,0).setAnchor(GBC.EAST)); //первая ячейка сверху слева (0,0) выровнено внутри ячейки (восток) 
+		add(face, new GBC(1, 0).setFill(GBC.HORIZONTAL).setWeight(100, 0).setInsets(1)); 
+		add(sizeLabel, new GBC(0, 1).setAnchor(GBC.EAST)); //вторая ячейка сверху слева (0,1) выровнено внутри ячейки (восток) 
 		add(size, new GBC(1, 1).setFill(GBC.HORIZONTAL).setWeight(100, 0).setInsets(1));
-		add(bold, new GBC(0, 2, 2,1).setAnchor(GBC.CENTER).setWeight(100, 100));
-		add(italic, new GBC(0, 3, 2,1).setAnchor(GBC.CENTER).setWeight(100, 100));
+		add(bold, new GBC(0, 2, 2,1).setAnchor(GBC.CENTER).setWeight(100, 100)); //вторая ячейка сверху слева (0,1) выровнено внутри ячейки (восток) 
+		add(italic, new GBC(0, 3, 2,1).setAnchor(GBC.CENTER).setWeight(100, 100));//вторая ячейка сверху слева (0,1) выровнено внутри ячейки (восток) 
 		add(sample, new GBC(2, 0, 1, 4).setFill(GBC.BOTH).setWeight(100, 100));
-		pack();
+		setSize(600, 400);  //размер фрейма
+		//pack();
 		setVisible(true);
-		updateSample();
+		updateSample(); 
 	}
 	
+	//метод обновления при действиях
+	
 	public void updateSample() {
-		String fontFace = (String)face.getSelectedItem();
-		int fontStyle = (bold.isSelected() ? Font.BOLD : 0) + (italic.isSelected() ? Font.ITALIC : 0);
-		int fontSize = size.getItemAt(size.getSelectedIndex());
-		Font font = new Font(fontFace, fontStyle, fontSize);
+		String fontFace = (String)face.getSelectedItem();   //название, которое выбрал позьзователь 
+		int fontStyle = (bold.isSelected() ? Font.BOLD : 0) + (italic.isSelected() ? Font.ITALIC : 0); // опр стиль
+		int fontSize = size.getItemAt(size.getSelectedIndex()); //размер, выбраный пользователем
+		Font font = new Font(fontFace, fontStyle, fontSize); //название шрифта, стиль, размер
 		sample.setFont(font);
 		sample.repaint();					
 	}
